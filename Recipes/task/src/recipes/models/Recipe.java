abbf5c6e6b0cd2e791d7077bb1c6645f3cb5db54
Junior Brito <junior.br.pr@gmail.com>
2023-04-20 00:30:47 +0200

    Added dependencies for stage 3

--- Recipes/task/src/recipes/models/Recipe.java
+++ Recipes/task/src/recipes/models/Recipe.java
@@ -1,6 +1,5 @@
 package recipes.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Recipe {
-    @JsonIgnore
     int id;
     String name;
     String description;
